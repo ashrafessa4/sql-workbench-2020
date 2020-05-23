c9be7455d895933044078377450276fef9fe4bdb
ashrafessa4 <62595154+ashrafessa4@users.noreply.github.com>
2020-05-23 22:21:17 +0300

    Update lesson9_homework.sql

--- lesson9_homework.sql
+++ lesson9_homework.sql
@@ -1,4 +1,10 @@
 1.
-SELECT salesman.name, customer.cust_name, customer.city 
-FROM salesman,customer 
-WHERE salesman.city=customer.city;
+  SELECT salesman.name, customer.cust_name, customer.city 
+  FROM salesman,customer 
+  WHERE salesman.city=customer.city;
+2.
+  select o.ord_no, o.purch_amt, c.cust_name, c.city
+  from orders as o, customer as c
+  where o.purch_amt between 500 and 2000 and o.customer_id=c.customer_id
+3.
+  
