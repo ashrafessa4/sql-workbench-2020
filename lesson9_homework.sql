fc2192cfd8c1407db570a2c8badfde4d3ba6fffe
ashrafessa4 <62595154+ashrafessa4@users.noreply.github.com>
2020-05-23 23:18:50 +0300

    Update lesson9_homework.sql

--- lesson9_homework.sql
+++ lesson9_homework.sql
@@ -18,11 +18,29 @@
   on c.salesman_id=s.salesman_id
   where s.commission > 0.12
 5.
+  select c.cust_name as customer, s.name as salesman, s.commission
+  from customer as c
+  join salesman as s
+  on c.salesman_id=s.salesman_id
+  where s.commission > 0.12 and c.city!=s.city
+6. 
   select o.ord_no, o.ord_date, o.purch_amt, s.name, s.commission, c.cust_name
   from orders as o
   join customer as c
   on o.customer_id=c.customer_id
   join salesman as s
   on s.salesman_id=o.salesman_id
-
+7.
+  select c.cust_name, s.name
+	from customer as c
+	join salesman as s
+  on c.salesman_id=s.salesman_id
+  order by c.customer_id asc;
+8. 
+  select c.cust_name, c.grade, s.name
+    from customer as c
+    join salesman as s
+    on c.salesman_id=s.salesman_id
+    where c.grade < 300
+    order by s.salesman_id asc
   
